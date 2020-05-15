595d14fd15df92072dbbc0291c74de35da573a2b
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-05-15 16:22:51 +0900

    page prop 대신 router 사용 - index page / document page 분기처리 -> 퍼블리싱 완료후 수정 예정

--- components/Layout.js
+++ components/Layout.js
@@ -1,37 +1,64 @@
-import Head from 'next/head';
-import Header from './Header';
-import Footer from './Footer';
+import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
 import React from "react";
 import { useRouter } from "next/router";
 import Aside from "./Aside";
 
 const Layout = props => {
+  const router = useRouter();
 
+  let page = "";
+  switch (router.pathname.split("/")[1]) {
+    case "":
+      page = "index";
+      break;
 
-    return (
-        <>
-            {/* 공통 head 영역 */}
-            <Head>
-                <title>무료 템플릿을 만나보세요. 부트스트랩 / 뷰 / 리액트 - My UI</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport"/>
-                <meta name="Referrer" content="origin"/>
-                <meta httpEquiv="X-UA-Compatible" content="IE=Edge"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    case "doc":
+      page = "document";
+      break;
 
-                {/* app 파비콘 설정 */}
-                <link rel="shortcut icon" type="image/x-icon" href="/img/common/favicon.ico"/>
+    default:
+      break;
+  }
 
-                <script src="/js/bundle.js"/>
-            </Head>
-            <div className={"wrap"}>
-                <Header/>
+  return (
+    <>
+      {/* 공통 head 영역 */}
+      <Head>
+        <title>
+          무료 템플릿을 만나보세요. 부트스트랩 / 뷰 / 리액트 - My UI
+        </title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+        <meta name="Referrer" content="origin" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-                {props.children}
-                <Footer/>
-            </div>
+        {/* app 파비콘 설정 */}
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="/img/common/favicon.ico"
+        />
 
-        </>
-    )
+        <script src="/js/bundle.js" />
+      </Head>
+      <div className={"wrap"}>
+        <Header />
+        {(() => {
+          if (page !== "index") {
+            return <Aside />;
+          }
+        })()}
+        {props.children}
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Layout;
