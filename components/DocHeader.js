88b457e936fde0e564e52c3aacc5f32d37987289
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-05-18 13:27:08 +0900

    Doc header - 링크 연동

--- components/DocHeader.js
+++ components/DocHeader.js
@@ -1,19 +1,38 @@
 import React, {useState} from 'react';
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 // Header
 const DocHeader = props => {
+
+    // TODO: nav-item document.html component.html 구분 필요한지
+
+    // const router = useRouter();
+    // const path = router.pathname.split('/')[2];
+
+    // switch(path) {
+    //     case 'document':
+    //         break;
+    //     case 'component':
+    //         break;
+    //     default:
+    //         console.log(404);
+    //         break;
+    // }
 
     return (
         <header className="doc-header">
             <nav className="navbar navbar-expand">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="index.html">MyUI Home<span
-                            className="sr-only">(current)</span></a>
+                        <Link href={"/"}>
+                            <a className="nav-link">MyUI Home<span className="sr-only">(current)</span></a>
+                        </Link>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" href="document.html">Documentation</a>
+                        <Link href={"/doc/document"}>
+                            <a className="nav-link">Documentation</a>
+                        </Link>
                     </li>
                 </ul>
             </nav>
