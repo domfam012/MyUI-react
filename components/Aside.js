01490326116828ffdf92ca497320b397b72a1ed6
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-05-18 11:50:47 +0900

    Doc aside - 링크 연동

--- components/Aside.js
+++ components/Aside.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import Link from "next/link";
 
 // aside
 const Aside = props => (
     <aside className="doc-aside">
         <header className="header bg-header">
-            <a className="navbar-brand" href="index.html"><img src="../img/common/logo.png" alt="MyUI"/></a>
+            <Link href={"/"}>
+                <a className="navbar-brand"><img src="../img/common/logo.png" alt="MyUI"/></a>
+            </Link>
         </header>
 
         <div className="box-snb">
             <div className="nav snb" id="navbar-example2">
-                <a className="h-title active" href="document.html"><i className="fas fa-bolt"></i>시작하기</a>
+
+                <Link href={"./document"}>
+                    <a className="h-title active"><i className="fas fa-bolt"/>시작하기</a>
+                </Link>
+
                 <div className="m-menu">
                     <a className="m-item nav-link" href="#intro">소개</a>
                     <a className="m-item nav-link" href="#download">다운로드</a>
                     <a className="m-item nav-link" href="#buildtool">빌드도구</a>
                     <a className="m-item nav-link" href="#device">브라우저&디바이스</a>
                 </div>
-                <a className="h-title" href="component.html"><i className="fas fa-th"></i>구성요소</a>
+
+                <Link href={"./component"}>
+                    <a className="h-title"><i className="fas fa-th"/>구성요소</a>
+                </Link>
+
                 <div className="m-menu">
                     <a className="m-item nav-link" href="#badge">배지(Badge)</a>
                     <a className="m-item nav-link" href="#breadcrumb">경로(Breadcrumb)</a>
